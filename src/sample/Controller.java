ab2dfb9e6feea31ae7d3c5fe64e22faffafdc3ce
sydykovislam <sydykovislam03@gmail.com>
2021-03-21 13:29:59 +0600

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,172 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author mrunal
+ */
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField tf,tf2 ;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button equals;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button clear;
+
+    @FXML
+    private Button plus;
+
+    @FXML
+    private Button mult;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button division;
+
+    private int operator ;
+    double a,b,result ;
+    String prevStr="";
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        String str = ((Labeled)event.getSource()).getText();
+        tf.setText(tf.getText()+str);
+        tf2.setText(prevStr+str);
+        prevStr = tf2.getText();
+    }
+
+    @FXML
+    private void add(){
+        try{
+            a = Double.parseDouble(tf.getText());
+            operator = 1 ;
+            tf2.setText(prevStr+"+");
+            prevStr = tf2.getText();
+        }catch(NumberFormatException e){
+            System.out.println("Enter value First");
+        }finally{
+            tf.setText("");
+        }
+    }
+
+    @FXML
+    private void substract(){
+        try{
+            a = Double.parseDouble(tf.getText());
+            operator = 2 ;
+            tf2.setText(prevStr+"-");
+            prevStr = tf2.getText();
+        }catch(NumberFormatException e){
+            System.out.println("Enter value First");
+        }finally{
+            tf.setText("");
+        }
+    }
+
+    @FXML
+    private void multiply(){
+        try{
+            a = Double.parseDouble(tf.getText());
+            operator = 3 ;
+            tf2.setText(prevStr+"*");
+            prevStr = tf2.getText();
+        }catch(NumberFormatException e){
+            System.out.println("Enter value First");
+        }finally{
+            tf.setText("");
+        }
+    }
+    @FXML
+    private void clearText(){
+        tf.setText("");
+        tf2.setText("");
+        prevStr="";
+    }
+
+    @FXML
+    private void divide(){
+        try{
+            a = Double.parseDouble(tf.getText());
+            operator = 4 ;
+            tf2.setText(prevStr+"/");
+            prevStr = tf2.getText();
+        }catch(NumberFormatException e){
+            System.out.println("Enter value First");
+        }finally{
+            tf.setText("");
+        }
+    }
+
+    @FXML
+    private void equals(){
+        try{
+            b = Double.parseDouble(tf.getText());
+
+            switch(operator){
+                case 1 : result = a + b ; tf.setText(""+result);  break;
+                case 2 : result = a - b ; tf.setText(""+result);  break;
+                case 3 : result = a * b ; tf.setText(""+result);  break;
+                case 4 : try{
+                    result = a/b ;
+                    tf.setText(""+result);
+
+                }catch(Exception e){
+                    tf.setText("Error");
+                }
+            }
+            tf2.setText(prevStr+"="+result);
+
+
+
+        }catch(NumberFormatException e){
+            System.out.println("Select values First");
+        }
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,33 @@
+package  sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author mrunal
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Calculator");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
